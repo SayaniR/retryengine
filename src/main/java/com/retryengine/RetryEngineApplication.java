414585968a73e5731625f00938d2a6511868f5a6
Sayani Roy <91311189+SayaniR@users.noreply.github.com>
2025-04-19 21:56:46 +0530

    Update RetryEngineApplication.java

--- src/main/java/com/retryengine/RetryEngineApplication.java
+++ src/main/java/com/retryengine/RetryEngineApplication.java
@@ -1,1 +1,11 @@
+package com.retryengine;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetryEngineApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RetryEngineApplication.class, args);
+	}
+}
